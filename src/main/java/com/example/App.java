36c8eea3b22f587a426400f86d0399017b57db9f
itokami1123dev <kamiari819dev@gmail.com>
2014-11-06 00:42:22 +0900

    Thymeleaf対応

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,20 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
-/**
- * Hello world!
- */
-@RestController
+@Controller
 @EnableAutoConfiguration
+@ComponentScan
+@Configuration
 public class App {
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
     }
